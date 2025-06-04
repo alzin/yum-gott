5fc01dc3fa4c7dd4aab83e0dc7b71fc6ac2526ce
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2025-06-04 19:35:55 +0300

    Separate tables from each other

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,34 +1,3 @@
-// export enum UserType {
-//     CUSTOMER = "customer",
-//     RESTAURANT_OWNER = "restaurant_owner"
-// }
-
-// export interface BaseUser {
-//     id?: string;
-//     mobileNumber: string;
-//     password: string;
-//     userType: UserType;
-//     isActive: boolean;
-//     createdAt?: Date;
-//     updatedAt?: Date;
-// };
-
-// export interface Customer extends BaseUser {
-//     name: string;
-//     email: string;
-//     userType: UserType.CUSTOMER;
-// }
-
-// export interface restaurantOwner extends BaseUser {
-//     restaurantName: string;
-//     organizationNumber: string;
-//     email: string; 
-//     userType: UserType.RESTAURANT_OWNER
-// }
-
-// export type User = Customer | restaurantOwner;
-
-
 
 export interface BaseUser {
     id?: string;
