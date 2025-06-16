79350f595540736efe76c24b257485f1b146006f
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2025-06-16 07:08:45 +0300

    IFileStorageService

--- src/application/interface/IFileStorageService.ts
+++ src/application/interface/IFileStorageService.ts
@@ -1,4 +1,6 @@
 export interface IFileStorageService {
     uploadFile(file: Express.Multer.File, userId: string, userType: 'customer' | 'restaurant_owner'): Promise<string>;
     deleteFile(fileUrl: string): Promise<void>;
+    uploadFile(file: Express.Multer.File, userId: string, userType: 'customer' | 'restaurant_owner' | 'product'): Promise<string>;
+    // deleteFile(fileUrl: string): Promise<void>;
 }
