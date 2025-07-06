a1dcc0ac586b65c6cba0beb89bb93588b9ed6a07
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2025-07-06 14:28:44 +0300

    add update to IProductRepository

--- src/domain/repositories/IProductOptionValueRepository.ts
+++ src/domain/repositories/IProductOptionValueRepository.ts
@@ -2,6 +2,7 @@ import { ProductOptionValue } from "../entities/ProductOptionValue";
 export interface IProductOptionValueRepository {
     create(value: ProductOptionValue): Promise<ProductOptionValue>;
     findById(valueId: string): Promise<ProductOptionValue | null>;
+    update(valueId: string, updates: Partial<ProductOptionValue>): Promise<ProductOptionValue>
     findByOptionId(optionId: string): Promise<ProductOptionValue[]>;
     delete(valueId: string): Promise<void>;
 }
