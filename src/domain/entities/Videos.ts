392f96bc25a3d845120d6d189c1e853a9d35a3d6
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2025-07-17 11:11:36 +0300

    add videos entites

---
+++ src/domain/entities/Videos.ts
@@ -0,0 +1,23 @@
+export enum VideoStatus {
+    PENDING = 'pending',
+    REJECTED = 'rejected',
+    ACCEPTED = 'accepted'
+}
+
+export enum Network {
+    MTN = 'MTN',
+    SYRIATEL = 'Syriatel'
+}
+
+export interface Video {
+    id?: string;
+    userId: string;
+    publicId: string;
+    secureUrl: string;
+    phoneNumber: string;
+    network: Network;
+    invoiceImage: string;
+    statusVideo: VideoStatus;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
