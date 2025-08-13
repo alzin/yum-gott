6366d844c11426409cc8df1456f30f5fefc25cf9
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2025-08-13 17:05:58 +0300

    handle production and development

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ process.on('SIGTERM', () => {
   console.log('SIGTERM received, shutting down gracefully');
   process.exit(0);
 }); 
-
+ 
 process.on('SIGINT', () => {
   console.log('SIGINT received, shutting down gracefully');
   process.exit(0);
