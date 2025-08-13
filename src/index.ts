684ad454f899287588730c39f6a073e070a52250
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2025-08-13 17:25:43 +0300

    handel prodution and development

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,11 @@ import { App } from './app';
 const nodeEnv = process.env.NODE_ENV || 'development';
 const envFilePath = `.env.${nodeEnv}`;
 dotenv.config({ path: envFilePath });
-// Create and start the application
 const app = new App();
 const port = parseInt(process.env.PORT || '3000', 10);
 
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
-// Graceful shutdown
 process.on('SIGTERM', () => {
   console.log('SIGTERM received, shutting down gracefully');
   process.exit(0);
